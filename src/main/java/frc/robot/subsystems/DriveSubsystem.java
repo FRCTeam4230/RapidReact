80bfad826b4f1d67d8b9764ab9fc8d8d7da19407
ZiyahX <93620681+ZiyahX@users.noreply.github.com>
2022-03-03 18:38:00 -0600

    Brake drivetrain motors

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -79,6 +79,7 @@ public class DriveSubsystem extends SubsystemBase {
 
     resetEncoders();
     setRampTime(DriveTrain.timeToFullSpeed);
+    setBraking(true);
 
     SmartDashboard.putData(this);
 
